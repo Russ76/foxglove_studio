Russ76/foxglove_studio

commit 41e3f14508f02921b9575ab4129345a817544bc7

Buffer messages from mcap sources as raw Uint8Arrays  (#7376)
**User-Facing Changes**
Store buffered messages as raw typed arrays to reduce memory pressure

**Description**
This PR aims to reduce OOM crashes by changing the playback buffer to
store messages as raw Uint8Arrays. Storing messages as Uint8arrays
reduces heap memory pressure as they do not count towards the heap
memory size compared to how deserialized messages (plain JS objects)
would. A positive consequence of this is, that we can buffer much more
messages than we were previously able to.

Whereas previously, messages would be deserialized in the worker, the
worker now sends (or rather transfers) undeserialized messages to the
main thread where they are stored in the playback buffer and
deserialized on the fly. The only downside of this approach is, that the
main thread now has to pay the cost of message deserialization. However,
this cost might be compensated by more efficient webworker communication
(due to the transferring of array buffers) and is worth to pay in return
for less OOM crashes and a larger playback buffer.

Note that for now the above was only implemented for MCAP related
sources (local/remote mcap files, nuscenes)

Supersedes #7267
Resolves FG-6094

packages/studio-base/src/players/IterablePlayer/DeserializingIterableSource.ts
@@ -1,0 +1,167 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { pickFields } from "@foxglove/den/records";
+import { parseChannel } from "@foxglove/mcap-support";
+import { MessageEvent } from "@foxglove/studio";
+import {
+  MessageIteratorArgs,
+  IteratorResult,
+  GetBackfillMessagesArgs,
+  IDeserializedIterableSource,
+  Initalization,
+  IIterableSource,
+} from "@foxglove/studio-base/players/IterablePlayer/IIterableSource";
+import { estimateObjectSize } from "@foxglove/studio-base/players/messageMemoryEstimation";
+
+/**
+ * Iterable source that deserializes messages from a raw iterable source (messages are Uint8Arrays).
+ */
+export class DeserializingIterableSource implements IDeserializedIterableSource {
+  #source: IIterableSource<Uint8Array>;
+  #deserializersByTopic: Record<string, (data: ArrayBufferView) => unknown> = {};
+  #messageSizeEstimateByTopic: Record<string, number> = {};
+  #connectionIdByTopic: Record<string, number> = {};
+
+  public readonly sourceType = "deserialized";
+
+  public constructor(source: IIterableSource<Uint8Array>) {
+    this.#source = source;
+  }
+
+  public async initialize(): Promise<Initalization> {
+    return this.initializeDeserializers(await this.#source.initialize());
+  }
+
+  public initializeDeserializers(initResult: Initalization): Initalization {
+    const problems: Initalization["problems"] = [];
+
+    let nextConnectionId = 0;
+    for (const {
+      name: topic,
+      messageEncoding,
+      schemaName,
+      schemaData,
+      schemaEncoding,
+    } of initResult.topics) {
+      this.#connectionIdByTopic[topic] = nextConnectionId++;
+
+      if (this.#deserializersByTopic[topic] == undefined) {
+        try {
+          if (messageEncoding == undefined) {
+            throw new Error(`Unspecified message encoding for topic ${topic}`);
+          }
+
+          const schema =
+            schemaName != undefined && schemaData != undefined && schemaEncoding != undefined
+              ? {
+                  name: schemaName,
+                  encoding: schemaEncoding,
+                  data: schemaData,
+                }
+              : undefined;
+
+          const { deserialize } = parseChannel({
+            messageEncoding,
+            schema,
+          });
+          this.#deserializersByTopic[topic] = deserialize;
+        } catch (error) {
+          // This should in practice never happen as the underlying source filters out invalid topics
+          problems.push({
+            severity: "error",
+            message: `Error in topic ${topic}: ${error.message}`,
+            error,
+          });
+        }
+      }
+    }
+
+    return { ...initResult, problems: initResult.problems.concat(problems) };
+  }
+
+  public messageIterator(
+    args: MessageIteratorArgs,
+  ): AsyncIterableIterator<Readonly<IteratorResult>> {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const self = this;
+    const rawIterator = self.#source.messageIterator(args);
+    return (async function* deserializedIterableGenerator() {
+      try {
+        for await (const iterResult of rawIterator) {
+          if (iterResult.type !== "message-event") {
+            yield iterResult;
+            continue;
+          }
+
+          try {
+            const fieldsToPick = args.topics.get(iterResult.msgEvent.topic)?.fields ?? [];
+            const deserializedMsgEvent = self.#deserializeMessage(
+              iterResult.msgEvent,
+              fieldsToPick,
+            );
+            yield {
+              type: iterResult.type,
+              msgEvent: deserializedMsgEvent,
+            };
+          } catch (err) {
+            const connectionId = self.#connectionIdByTopic[iterResult.msgEvent.topic] ?? 0;
+            yield {
+              type: "problem",
+              connectionId,
+              problem: {
+                severity: "error",
+                message: `Failed to deserialize message on topic ${
+                  iterResult.msgEvent.topic
+                }. ${err.toString()}`,
+                tip: `Check that your input file is not corrupted.`,
+              },
+            };
+          }
+        }
+      } finally {
+        await rawIterator.return?.();
+      }
+    })();
+  }
+
+  public async getBackfillMessages(args: GetBackfillMessagesArgs): Promise<MessageEvent[]> {
+    const deserialize = (rawMessages: MessageEvent<Uint8Array>[]) => {
+      return rawMessages.map((rawMsg) => {
+        const fieldsToPick = args.topics.get(rawMsg.topic)?.fields ?? [];
+        return this.#deserializeMessage(rawMsg, fieldsToPick);
+      });
+    };
+    return await this.#source.getBackfillMessages(args).then(deserialize);
+  }
+
+  #deserializeMessage(
+    rawMessageEvent: MessageEvent<Uint8Array>,
+    fieldsToPick: string[],
+  ): MessageEvent {
+    const { topic, message } = rawMessageEvent;
+
+    const deserialize = this.#deserializersByTopic[topic];
+    if (!deserialize) {
+      throw new Error(`Failed to find deserializer for topic ${topic}`);
+    }
+
+    const deserializedMessage = deserialize(message) as Record<string, unknown>;
+    const msg =
+      fieldsToPick.length > 0 ? pickFields(deserializedMessage, fieldsToPick) : deserializedMessage;
+
+    // Lookup the size estimate for this topic or compute it if not found in the cache.
+    let msgSizeEstimate = this.#messageSizeEstimateByTopic[rawMessageEvent.topic];
+    if (msgSizeEstimate == undefined) {
+      msgSizeEstimate = estimateObjectSize(msg);
+      this.#messageSizeEstimateByTopic[rawMessageEvent.topic] = msgSizeEstimate;
+    }
+
+    return {
+      ...rawMessageEvent,
+      message: msg,
+      sizeInBytes: Math.max(message.byteLength, msgSizeEstimate),
+    };
+  }
+}

benchmark/src/dataSources/McapLocalBenchmarkDataSourceFactory.ts
@@ -6,6 +6,7 @@ import {
   IDataSourceFactory,
   DataSourceFactoryInitializeArgs,
 } from "@foxglove/studio-base/context/PlayerSelectionContext";
+import { DeserializingIterableSource } from "@foxglove/studio-base/players/IterablePlayer/DeserializingIterableSource";
 import { McapIterableSource } from "@foxglove/studio-base/players/IterablePlayer/Mcap/McapIterableSource";
 import { Player } from "@foxglove/studio-base/players/types";
 
@@ -25,7 +26,8 @@ class McapLocalBenchmarkDataSourceFactory implements IDataSourceFactory {
     }
 
     const mcapProvider = new McapIterableSource({ type: "file", file });
-    return new BenchmarkPlayer(file.name, mcapProvider);
+    const source = new DeserializingIterableSource(mcapProvider);
+    return new BenchmarkPlayer(file.name, source);
   }
 }
 

benchmark/src/players/BenchmarkPlayer.ts
@@ -8,7 +8,7 @@ import { toRFC3339String } from "@foxglove/rostime";
 import { MessageEvent } from "@foxglove/studio";
 import { GlobalVariables } from "@foxglove/studio-base/hooks/useGlobalVariables";
 import { BlockLoader } from "@foxglove/studio-base/players/IterablePlayer/BlockLoader";
-import { IIterableSource } from "@foxglove/studio-base/players/IterablePlayer/IIterableSource";
+import { IDeserializedIterableSource } from "@foxglove/studio-base/players/IterablePlayer/IIterableSource";
 import PlayerProblemManager from "@foxglove/studio-base/players/PlayerProblemManager";
 import {
   AdvertiseOptions,
@@ -30,14 +30,14 @@ const MAX_BLOCKS = 400;
 const CAPABILITIES: string[] = [PlayerCapabilities.playbackControl];
 
 class BenchmarkPlayer implements Player {
-  #source: IIterableSource;
+  #source: IDeserializedIterableSource;
   #name: string;
   #listener?: (state: PlayerState) => Promise<void>;
   #subscriptions: SubscribePayload[] = [];
   #blockLoader?: BlockLoader;
   #problemManager = new PlayerProblemManager();
 
-  public constructor(name: string, source: IIterableSource) {
+  public constructor(name: string, source: IDeserializedIterableSource) {
     this.#name = name;
     this.#source = source;
   }

packages/studio-base/src/dataSources/McapLocalDataSourceFactory.ts
@@ -3,10 +3,11 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import {
-  IDataSourceFactory,
   DataSourceFactoryInitializeArgs,
+  IDataSourceFactory,
 } from "@foxglove/studio-base/context/PlayerSelectionContext";
-import { IterablePlayer, WorkerIterableSource } from "@foxglove/studio-base/players/IterablePlayer";
+import { IterablePlayer } from "@foxglove/studio-base/players/IterablePlayer";
+import { WorkerRawIterableSource } from "@foxglove/studio-base/players/IterablePlayer/WorkerRawIterableSource";
 import { Player } from "@foxglove/studio-base/players/types";
 
 class McapLocalDataSourceFactory implements IDataSourceFactory {
@@ -22,7 +23,7 @@ class McapLocalDataSourceFactory implements IDataSourceFactory {
       return;
     }
 
-    const source = new WorkerIterableSource({
+    const source = new WorkerRawIterableSource({
       initWorker: () => {
         return new Worker(
           // foxglove-depcheck-used: babel-plugin-transform-import-meta

packages/studio-base/src/dataSources/RemoteDataSourceFactory.tsx
@@ -6,10 +6,14 @@ import { Link } from "@mui/material";
 import path from "path";
 
 import {
-  IDataSourceFactory,
   DataSourceFactoryInitializeArgs,
+  IDataSourceFactory,
 } from "@foxglove/studio-base/context/PlayerSelectionContext";
-import { IterablePlayer, WorkerIterableSource } from "@foxglove/studio-base/players/IterablePlayer";
+import {
+  IterablePlayer,
+  WorkerIterableSource,
+  WorkerRawIterableSource,
+} from "@foxglove/studio-base/players/IterablePlayer";
 import { Player } from "@foxglove/studio-base/players/types";
 
 const initWorkers: Record<string, () => Worker> = {
@@ -93,8 +97,10 @@ class RemoteDataSourceFactory implements IDataSourceFactory {
       throw new Error(`Unsupported extension: ${extension}`);
     }
 
-    const source = new WorkerIterableSource({ initWorker, initArgs: { url } });
-
+    const source =
+      extension === ".mcap"
+        ? new WorkerRawIterableSource({ initWorker, initArgs: { url } })
+        : new WorkerIterableSource({ initWorker, initArgs: { url } });
     return new IterablePlayer({
       source,
       name: url,

packages/studio-base/src/dataSources/SampleNuscenesDataSourceFactory.ts
@@ -3,10 +3,11 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import {
-  IDataSourceFactory,
   DataSourceFactoryInitializeArgs,
+  IDataSourceFactory,
 } from "@foxglove/studio-base/context/PlayerSelectionContext";
-import { IterablePlayer, WorkerIterableSource } from "@foxglove/studio-base/players/IterablePlayer";
+import { IterablePlayer } from "@foxglove/studio-base/players/IterablePlayer";
+import { WorkerRawIterableSource } from "@foxglove/studio-base/players/IterablePlayer/WorkerRawIterableSource";
 
 import SampleNuscenesLayout from "./SampleNuscenesLayout.json";
 
@@ -23,7 +24,7 @@ class SampleNuscenesDataSourceFactory implements IDataSourceFactory {
   ): ReturnType<IDataSourceFactory["initialize"]> {
     const bagUrl = "https://assets.foxglove.dev/NuScenes-v1.0-mini-scene-0061-df24c12.mcap";
 
-    const source = new WorkerIterableSource({
+    const source = new WorkerRawIterableSource({
       initWorker: () => {
         return new Worker(
           // foxglove-depcheck-used: babel-plugin-transform-import-meta

packages/studio-base/src/players/IterablePlayer/BlockLoader.test.ts
@@ -10,13 +10,14 @@ import { mockTopicSelection } from "@foxglove/studio-base/test/mocks/mockTopicSe
 import { BlockLoader, MEMORY_INFO_PRELOADED_MSGS } from "./BlockLoader";
 import {
   GetBackfillMessagesArgs,
-  IIterableSource,
+  IDeserializedIterableSource,
   Initalization,
   IteratorResult,
   MessageIteratorArgs,
 } from "./IIterableSource";
 
-class TestSource implements IIterableSource {
+class TestSource implements IDeserializedIterableSource {
+  public readonly sourceType = "deserialized";
   public async initialize(): Promise<Initalization> {
     return {
       start: { sec: 0, nsec: 0 },

packages/studio-base/src/players/IterablePlayer/BlockLoader.ts
@@ -21,7 +21,7 @@ import { IteratorCursor } from "@foxglove/studio-base/players/IterablePlayer/Ite
 import PlayerProblemManager from "@foxglove/studio-base/players/PlayerProblemManager";
 import { MessageBlock, Progress, TopicSelection } from "@foxglove/studio-base/players/types";
 
-import { IIterableSource, MessageIteratorArgs } from "./IIterableSource";
+import { IDeserializedIterableSource, MessageIteratorArgs } from "./IIterableSource";
 
 const log = Log.getLogger(__filename);
 
@@ -29,7 +29,7 @@ export const MEMORY_INFO_PRELOADED_MSGS = "Preloaded messages";
 
 type BlockLoaderArgs = {
   cacheSizeBytes: number;
-  source: IIterableSource;
+  source: IDeserializedIterableSource;
   start: Time;
   end: Time;
   maxBlocks: number;
@@ -51,7 +51,7 @@ type LoadArgs = {
  * BlockLoader manages loading blocks from a source. Blocks are fixed time span containers for messages.
  */
 export class BlockLoader {
-  #source: IIterableSource;
+  #source: IDeserializedIterableSource;
   #blocks: Blocks = [];
   #start: Time;
   #end: Time;

commit 0a1428ed305ca5b9f5be3ee38326709d22b0b596

Fix memory estimation not differentiating between normal and sliced messages (#7442)
**User-Facing Changes**
None (as #7376 wasn't released yet)

**Description**
Fixes a regression introduced with #7376 regarding message size
estimation. The message size estimation did not take into account
potentially sliced messages which could lead to the message size
estimate being far from reality. We previously solved this by using a
subscription hash (unique hash for topic + subscribed fields). This
subscription hash logic was wrongly removed in #7376 and this PR adds it
back (at a different place though). It should be mentioned that with the
architectural changes introduced with #7376 message slicing will work
for all serialized sources, not only the mcap source.

packages/studio-base/src/players/IterablePlayer/DeserializingIterableSource.ts
@@ -14,6 +14,15 @@ import {
   IIterableSource,
 } from "@foxglove/studio-base/players/IterablePlayer/IIterableSource";
 import { estimateObjectSize } from "@foxglove/studio-base/players/messageMemoryEstimation";
+import { SubscribePayload } from "@foxglove/studio-base/players/types";
+
+// Computes the subscription hash for a given topic & subscription payload pair.
+// In the simplest case, when there are no message slicing fields, the subscription hash is just
+// the topic name. If there are slicing fields, the hash is computed as the topic name appended
+// by "+" seperated message slicing fields.
+function computeSubscriptionHash(topic: string, subscribePayload: SubscribePayload): string {
+  return subscribePayload.fields ? topic + "+" + subscribePayload.fields.join("+") : topic;
+}
 
 /**
  * Iterable source that deserializes messages from a raw iterable source (messages are Uint8Arrays).
@@ -21,7 +30,7 @@ import { estimateObjectSize } from "@foxglove/studio-base/players/messageMemoryE
 export class DeserializingIterableSource implements IDeserializedIterableSource {
   #source: IIterableSource<Uint8Array>;
   #deserializersByTopic: Record<string, (data: ArrayBufferView) => unknown> = {};
-  #messageSizeEstimateByTopic: Record<string, number> = {};
+  #messageSizeEstimateBySubHash: Record<string, number> = {};
   #connectionIdByTopic: Record<string, number> = {};
 
   public readonly sourceType = "deserialized";
@@ -84,6 +93,19 @@ export class DeserializingIterableSource implements IDeserializedIterableSource 
   public messageIterator(
     args: MessageIteratorArgs,
   ): AsyncIterableIterator<Readonly<IteratorResult>> {
+    // Compute the unique subscription hash for every topic + subscription payload pair which will
+    // be used to lookup message size estimates. This is done here to avoid having to compute the
+    // the subscription hash for every new message event.
+    const subscribePayloadWithHashByTopic = new Map(
+      Array.from(args.topics, ([topic, subscribePayload]) => [
+        topic,
+        {
+          ...subscribePayload,
+          subscriptionHash: computeSubscriptionHash(topic, subscribePayload),
+        },
+      ]),
+    );
+
     // eslint-disable-next-line @typescript-eslint/no-this-alias
     const self = this;
     const rawIterator = self.#source.messageIterator(args);
@@ -96,10 +118,16 @@ export class DeserializingIterableSource implements IDeserializedIterableSource 
           }
 
           try {
-            const fieldsToPick = args.topics.get(iterResult.msgEvent.topic)?.fields ?? [];
+            const subscription = subscribePayloadWithHashByTopic.get(iterResult.msgEvent.topic);
+            if (!subscription) {
+              throw new Error(
+                `Received message on topic ${iterResult.msgEvent.topic} which was not subscribed to.`,
+              );
+            }
+
             const deserializedMsgEvent = self.#deserializeMessage(
               iterResult.msgEvent,
-              fieldsToPick,
+              subscription,
             );
             yield {
               type: iterResult.type,
@@ -127,10 +155,26 @@ export class DeserializingIterableSource implements IDeserializedIterableSource 
   }
 
   public async getBackfillMessages(args: GetBackfillMessagesArgs): Promise<MessageEvent[]> {
+    // Compute the unique subscription hash for every topic + subscription payload pair which will
+    // be used to lookup message size estimates. This is done here to avoid having to compute the
+    // the subscription hash for every new message event.
+    const subscribePayloadWithHashByTopic = new Map(
+      Array.from(args.topics, ([topic, subscribePayload]) => [
+        topic,
+        {
+          ...subscribePayload,
+          subscriptionHash: computeSubscriptionHash(topic, subscribePayload),
+        },
+      ]),
+    );
+
     const deserialize = (rawMessages: MessageEvent<Uint8Array>[]) => {
       return rawMessages.map((rawMsg) => {
-        const fieldsToPick = args.topics.get(rawMsg.topic)?.fields ?? [];
-        return this.#deserializeMessage(rawMsg, fieldsToPick);
+        const subscription = subscribePayloadWithHashByTopic.get(rawMsg.topic);
+        if (!subscription) {
+          throw new Error(`Received message on topic ${rawMsg.topic} which was not subscribed to.`);
+        }
+        return this.#deserializeMessage(rawMsg, subscription);
       });
     };
     return await this.#source.getBackfillMessages(args).then(deserialize);
@@ -138,7 +182,7 @@ export class DeserializingIterableSource implements IDeserializedIterableSource 
 
   #deserializeMessage(
     rawMessageEvent: MessageEvent<Uint8Array>,
-    fieldsToPick: string[],
+    subscription: SubscribePayload & { subscriptionHash: string },
   ): MessageEvent {
     const { topic, message } = rawMessageEvent;
 
@@ -148,20 +192,27 @@ export class DeserializingIterableSource implements IDeserializedIterableSource 
     }
 
     const deserializedMessage = deserialize(message) as Record<string, unknown>;
-    const msg =
-      fieldsToPick.length > 0 ? pickFields(deserializedMessage, fieldsToPick) : deserializedMessage;
+    const msg = subscription.fields
+      ? pickFields(deserializedMessage, subscription.fields)
+      : deserializedMessage;
 
-    // Lookup the size estimate for this topic or compute it if not found in the cache.
-    let msgSizeEstimate = this.#messageSizeEstimateByTopic[rawMessageEvent.topic];
+    // Lookup the size estimate for this subscription hash or compute it if not found in the cache.
+    let msgSizeEstimate = this.#messageSizeEstimateBySubHash[subscription.subscriptionHash];
     if (msgSizeEstimate == undefined) {
       msgSizeEstimate = estimateObjectSize(msg);
-      this.#messageSizeEstimateByTopic[rawMessageEvent.topic] = msgSizeEstimate;
+      this.#messageSizeEstimateBySubHash[subscription.subscriptionHash] = msgSizeEstimate;
     }
+
+    // For sliced messages we use the estimated message size whereas for non-sliced messages
+    // take whatever size is bigger.
+    const sizeInBytes = subscription.fields
+      ? msgSizeEstimate
+      : Math.max(message.byteLength, msgSizeEstimate);
 
     return {
       ...rawMessageEvent,
       message: msg,
-      sizeInBytes: Math.max(message.byteLength, msgSizeEstimate),
+      sizeInBytes,
     };
   }
 }

packages/studio-base/src/players/IterablePlayer/DeserializingIterableSource.test.ts
@@ -1,0 +1,199 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { MessageEvent } from "@foxglove/studio";
+import {
+  GetBackfillMessagesArgs,
+  Initalization,
+  MessageIteratorArgs,
+  IteratorResult,
+  IRawIterableSource,
+} from "@foxglove/studio-base/players/IterablePlayer/IIterableSource";
+import { estimateObjectSize } from "@foxglove/studio-base/players/messageMemoryEstimation";
+
+import { DeserializingIterableSource } from "./DeserializingIterableSource";
+
+const textEncoder = new TextEncoder();
+
+async function* defaultMessageIterator(
+  _args: MessageIteratorArgs,
+): AsyncIterableIterator<Readonly<IteratorResult<Uint8Array>>> {
+  for (let i = 0; i < 8; ++i) {
+    const message = textEncoder.encode(JSON.stringify({ foo: "bar", iteration: i }));
+    yield {
+      type: "message-event",
+      msgEvent: {
+        topic: "json_topic",
+        receiveTime: { sec: 0, nsec: i * 1e8 },
+        message,
+        sizeInBytes: message.byteLength,
+        schemaName: "some_type",
+      },
+    };
+  }
+}
+
+class TestSource implements IRawIterableSource {
+  public readonly sourceType = "serialized";
+
+  public async initialize(): Promise<Initalization> {
+    return {
+      start: { sec: 0, nsec: 0 },
+      end: { sec: 10, nsec: 0 },
+      topics: [
+        {
+          name: "json_topic",
+          schemaName: "some_type",
+          messageEncoding: "json",
+        },
+      ],
+      topicStats: new Map(),
+      profile: undefined,
+      problems: [],
+      datatypes: new Map(),
+      publishersByTopic: new Map(),
+    };
+  }
+
+  public async *messageIterator(
+    _args: MessageIteratorArgs,
+  ): AsyncIterableIterator<Readonly<IteratorResult<Uint8Array>>> {}
+
+  public async getBackfillMessages(
+    _args: GetBackfillMessagesArgs,
+  ): Promise<MessageEvent<Uint8Array>[]> {
+    return [];
+  }
+}
+
+describe("DeserializingIterableSources", () => {
+  it("should construct and initialize", async () => {
+    const source = new TestSource();
+    const deserSource = new DeserializingIterableSource(source);
+
+    const initResult = await deserSource.initialize();
+    expect(initResult.problems).toStrictEqual([]);
+  });
+
+  it("deserializes messages from raw bytes", async () => {
+    const source = new TestSource();
+    const deserSource = new DeserializingIterableSource(source);
+    await deserSource.initialize();
+
+    source.messageIterator = defaultMessageIterator;
+    const messageIterator = deserSource.messageIterator({
+      topics: new Map([["json_topic", { topic: "json_topic" }]]),
+    });
+
+    for (let i = 0; i < 8; ++i) {
+      const iterResult = messageIterator.next();
+      await expect(iterResult).resolves.toEqual({
+        done: false,
+        value: {
+          type: "message-event",
+          msgEvent: {
+            receiveTime: { sec: 0, nsec: i * 1e8 },
+            message: { foo: "bar", iteration: i },
+            sizeInBytes: 36,
+            topic: "json_topic",
+            schemaName: "some_type",
+          },
+        },
+      });
+    }
+
+    // The message iterator should be done since we have no more data to read from the source
+    const iterResult = messageIterator.next();
+    await expect(iterResult).resolves.toEqual({
+      done: true,
+    });
+  });
+
+  it("performs message slicing", async () => {
+    const source = new TestSource();
+    const deserSource = new DeserializingIterableSource(source);
+    await deserSource.initialize();
+
+    source.messageIterator = defaultMessageIterator;
+    const messageIterator = deserSource.messageIterator({
+      topics: new Map([["json_topic", { topic: "json_topic", fields: ["iteration"] }]]),
+    });
+    const slicedMessageSizeEstimate = estimateObjectSize({ iteration: 1 });
+
+    for (let i = 0; i < 8; ++i) {
+      const iterResult = messageIterator.next();
+      await expect(iterResult).resolves.toEqual({
+        done: false,
+        value: {
+          type: "message-event",
+          msgEvent: {
+            receiveTime: { sec: 0, nsec: i * 1e8 },
+            message: { iteration: i },
+            sizeInBytes: slicedMessageSizeEstimate,
+            topic: "json_topic",
+            schemaName: "some_type",
+          },
+        },
+      });
+    }
+
+    // The message iterator should be done since we have no more data to read from the source
+    const iterResult = messageIterator.next();
+    await expect(iterResult).resolves.toEqual({
+      done: true,
+    });
+  });
+
+  it("correctly estimates message sizes for sliced and non-sliced messages", async () => {
+    const source = new TestSource();
+    const deserSource = new DeserializingIterableSource(source);
+    await deserSource.initialize();
+
+    source.messageIterator = defaultMessageIterator;
+
+    const nonslicedMessageIterator = deserSource.messageIterator({
+      topics: new Map([["json_topic", { topic: "json_topic" }]]),
+    });
+    const slicedMessageIterator = deserSource.messageIterator({
+      topics: new Map([["json_topic", { topic: "json_topic", fields: ["foo"] }]]),
+    });
+    const slicedMessageSizeEstimate = estimateObjectSize({ foo: "bar" });
+
+    for (let i = 0; i < 8; ++i) {
+      const iterResult = nonslicedMessageIterator.next();
+      await expect(iterResult).resolves.toMatchObject({
+        done: false,
+        value: {
+          type: "message-event",
+          msgEvent: {
+            message: { foo: "bar", iteration: i },
+            sizeInBytes: 36,
+          },
+        },
+      });
+    }
+
+    for (let i = 0; i < 8; ++i) {
+      const iterResult = slicedMessageIterator.next();
+      await expect(iterResult).resolves.toMatchObject({
+        done: false,
+        value: {
+          type: "message-event",
+          msgEvent: {
+            message: { foo: "bar" },
+            sizeInBytes: slicedMessageSizeEstimate,
+          },
+        },
+      });
+    }
+
+    // Both message iterators should be done since we have no more data to read from the source
+    await expect(nonslicedMessageIterator.next()).resolves.toEqual({
+      done: true,
+    });
+    await expect(slicedMessageIterator.next()).resolves.toEqual({
+      done: true,
+    });
+  });
+});