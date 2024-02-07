Russ76/foxglove_studio

commit 3f95f0f0cdb47a8bd7cb9a4120fe1fae02952912

Buffer messages of bag & db3 sources as raw uint8arrays (#7420)
**User-Facing Changes**
Store buffered messages as raw bytes to reduce javascript heap memory
pressure (bag + db3)

**Description**
Similar to #7376 this PR converts the bag (ROS1) and db3 (ROS2) sources
to raw sources which allows to buffer messages as Uint8Arrays. This
Improves worker communication performance and reduces heap memory
pressure.

Resolves FG-6464

packages/studio-base/src/players/IterablePlayer/rosdb3/RosDb3IterableSource.ts
@@ -6,31 +6,27 @@ import { ROS2_TO_DEFINITIONS, Rosbag2, SqliteSqljs } from "@foxglove/rosbag2-web
 import { stringify } from "@foxglove/rosmsg";
 import { Time, add as addTime } from "@foxglove/rostime";
 import { MessageEvent } from "@foxglove/studio";
-import { estimateObjectSize } from "@foxglove/studio-base/players/messageMemoryEstimation";
-import {
-  MessageDefinitionsByTopic,
-  ParsedMessageDefinitionsByTopic,
-  PlayerProblem,
-  Topic,
-  TopicStats,
-} from "@foxglove/studio-base/players/types";
+import { PlayerProblem, TopicStats } from "@foxglove/studio-base/players/types";
 import { RosDatatypes } from "@foxglove/studio-base/types/RosDatatypes";
 import { basicDatatypes } from "@foxglove/studio-base/util/basicDatatypes";
 
 import {
-  IIterableSource,
-  IteratorResult,
-  Initalization,
-  MessageIteratorArgs,
   GetBackfillMessagesArgs,
+  ISerializedIterableSource,
+  Initalization,
+  IteratorResult,
+  MessageIteratorArgs,
+  TopicWithDecodingInfo,
 } from "../IIterableSource";
 
-export class RosDb3IterableSource implements IIterableSource {
+export class RosDb3IterableSource implements ISerializedIterableSource {
   #files: File[];
   #bag?: Rosbag2;
   #start: Time = { sec: 0, nsec: 0 };
   #end: Time = { sec: 0, nsec: 0 };
-  #messageSizeEstimateByTopic: Record<string, number> = {};
+  #textEncoder = new TextEncoder();
+
+  public readonly sourceType = "serialized";
 
   public constructor(files: File[]) {
     this.#files = files;
@@ -64,17 +60,20 @@ export class RosDb3IterableSource implements IIterableSource {
     }
 
     const problems: PlayerProblem[] = [];
-    const topics: Topic[] = [];
+    const topics: TopicWithDecodingInfo[] = [];
     const topicStats = new Map<string, TopicStats>();
     // ROS 2 .db3 files do not contain message definitions, so we can only support well-known ROS types.
     const datatypes: RosDatatypes = new Map([...ROS2_TO_DEFINITIONS, ...basicDatatypes]);
-    const messageDefinitionsByTopic: MessageDefinitionsByTopic = {};
-    const parsedMessageDefinitionsByTopic: ParsedMessageDefinitionsByTopic = {};
 
     for (const topicDef of topicDefs) {
       const numMessages = messageCounts.get(topicDef.name);
 
-      topics.push({ name: topicDef.name, schemaName: topicDef.type });
+      const topic: TopicWithDecodingInfo = {
+        name: topicDef.name,
+        schemaName: topicDef.type,
+        messageEncoding: topicDef.serializationFormat,
+      };
+
       if (numMessages != undefined) {
         topicStats.set(topicDef.name, { numMessages });
       }
@@ -86,14 +85,30 @@ export class RosDb3IterableSource implements IIterableSource {
           message: `Topic "${topicDef.name}" has unsupported datatype "${topicDef.type}"`,
           tip: "ROS 2 .db3 files do not contain message definitions, so only well-known ROS types are supported in Foxglove Studio. As a workaround, you can convert the db3 file to mcap using the mcap CLI. For more information, see: https://docs.foxglove.dev/docs/connecting-to-data/frameworks/ros2",
         });
-        continue;
-      }
+      } else {
+        // Create the full gendeps-like message definition so that parseChannel() can parse it.
+        const typesToProcess = [parsedMsgdef];
+        const typesForMessage: RosDatatypes = new Map();
+        while (typesToProcess.length > 0) {
+          const rosType = typesToProcess.shift()!;
+          for (const { type, isComplex } of rosType.definitions) {
+            if (isComplex === true && !typesForMessage.has(type)) {
+              const newComplexType = datatypes.get(type);
+              if (!newComplexType) {
+                // Should in theory never happen as these are all well-known types
+                throw new Error(`invariant: Subtype ${type} of type ${rosType.name} not found.`);
+              }
+              typesToProcess.push(newComplexType);
+              typesForMessage.set(type, newComplexType);
+            }
+          }
+        }
 
-      const fullParsedMessageDefinitions = [parsedMsgdef];
-      const messageDefinition = stringify(fullParsedMessageDefinitions);
-      datatypes.set(topicDef.type, { name: topicDef.type, definitions: parsedMsgdef.definitions });
-      messageDefinitionsByTopic[topicDef.name] = messageDefinition;
-      parsedMessageDefinitionsByTopic[topicDef.name] = fullParsedMessageDefinitions;
+        const messageDefinition = stringify(Array.from(typesForMessage.values()));
+        topic.schemaData = this.#textEncoder.encode(messageDefinition);
+        topic.schemaEncoding = "ros2msg";
+        topics.push(topic);
+      }
     }
 
     this.#start = start;
@@ -113,7 +128,7 @@ export class RosDb3IterableSource implements IIterableSource {
 
   public async *messageIterator(
     opt: MessageIteratorArgs,
-  ): AsyncIterableIterator<Readonly<IteratorResult>> {
+  ): AsyncIterableIterator<Readonly<IteratorResult<Uint8Array>>> {
     if (this.#bag == undefined) {
       throw new Error(`Rosbag2DataProvider is not initialized`);
     }
@@ -133,29 +148,25 @@ export class RosDb3IterableSource implements IIterableSource {
       startTime: start,
       endTime: inclusiveEndTime,
       topics: Array.from(topics.keys()),
+      rawMessages: true,
     });
     for await (const msg of msgIterator) {
-      // Lookup the size estimate for this topic or compute it if not found in the cache.
-      let msgSizeEstimate = this.#messageSizeEstimateByTopic[msg.topic.name];
-      if (msgSizeEstimate == undefined) {
-        msgSizeEstimate = estimateObjectSize(msg.value);
-        this.#messageSizeEstimateByTopic[msg.topic.name] = msgSizeEstimate;
-      }
-
       yield {
         type: "message-event",
         msgEvent: {
           topic: msg.topic.name,
           receiveTime: msg.timestamp,
-          message: msg.value,
-          sizeInBytes: Math.max(msg.data.byteLength, msgSizeEstimate),
+          message: msg.data,
+          sizeInBytes: msg.data.byteLength,
           schemaName: msg.topic.type,
         },
       };
     }
   }
 
-  public async getBackfillMessages(_args: GetBackfillMessagesArgs): Promise<MessageEvent[]> {
+  public async getBackfillMessages(
+    _args: GetBackfillMessagesArgs,
+  ): Promise<MessageEvent<Uint8Array>[]> {
     return [];
   }
 }

packages/studio-base/src/dataSources/RemoteDataSourceFactory.tsx
@@ -11,7 +11,6 @@ import {
 } from "@foxglove/studio-base/context/PlayerSelectionContext";
 import {
   IterablePlayer,
-  WorkerIterableSource,
   WorkerRawIterableSource,
 } from "@foxglove/studio-base/players/IterablePlayer";
 import { Player } from "@foxglove/studio-base/players/types";
@@ -97,10 +96,7 @@ class RemoteDataSourceFactory implements IDataSourceFactory {
       throw new Error(`Unsupported extension: ${extension}`);
     }
 
-    const source =
-      extension === ".mcap"
-        ? new WorkerRawIterableSource({ initWorker, initArgs: { url } })
-        : new WorkerIterableSource({ initWorker, initArgs: { url } });
+    const source = new WorkerRawIterableSource({ initWorker, initArgs: { url } });
     return new IterablePlayer({
       source,
       name: url,

packages/studio-base/src/dataSources/Ros1LocalBagDataSourceFactory.ts
@@ -6,7 +6,10 @@ import {
   IDataSourceFactory,
   DataSourceFactoryInitializeArgs,
 } from "@foxglove/studio-base/context/PlayerSelectionContext";
-import { IterablePlayer, WorkerIterableSource } from "@foxglove/studio-base/players/IterablePlayer";
+import {
+  IterablePlayer,
+  WorkerRawIterableSource,
+} from "@foxglove/studio-base/players/IterablePlayer";
 import { Player } from "@foxglove/studio-base/players/types";
 
 class Ros1LocalBagDataSourceFactory implements IDataSourceFactory {
@@ -22,7 +25,7 @@ class Ros1LocalBagDataSourceFactory implements IDataSourceFactory {
       return;
     }
 
-    const source = new WorkerIterableSource({
+    const source = new WorkerRawIterableSource({
       initWorker: () => {
         return new Worker(
           // foxglove-depcheck-used: babel-plugin-transform-import-meta

packages/studio-base/src/dataSources/Ros2LocalBagDataSourceFactory.ts
@@ -6,7 +6,10 @@ import {
   IDataSourceFactory,
   DataSourceFactoryInitializeArgs,
 } from "@foxglove/studio-base/context/PlayerSelectionContext";
-import { IterablePlayer, WorkerIterableSource } from "@foxglove/studio-base/players/IterablePlayer";
+import {
+  IterablePlayer,
+  WorkerRawIterableSource,
+} from "@foxglove/studio-base/players/IterablePlayer";
 import { Player } from "@foxglove/studio-base/players/types";
 
 class Ros2LocalBagDataSourceFactory implements IDataSourceFactory {
@@ -25,7 +28,7 @@ class Ros2LocalBagDataSourceFactory implements IDataSourceFactory {
       return;
     }
 
-    const source = new WorkerIterableSource({
+    const source = new WorkerRawIterableSource({
       initWorker: () => {
         return new Worker(
           // foxglove-depcheck-used: babel-plugin-transform-import-meta

packages/studio-base/src/players/IterablePlayer/BagIterableSource.ts
@@ -5,15 +5,8 @@
 import { Bag, Filelike } from "@foxglove/rosbag";
 import { BlobReader } from "@foxglove/rosbag/web";
 import { parse as parseMessageDefinition } from "@foxglove/rosmsg";
-import { MessageReader } from "@foxglove/rosmsg-serialization";
 import { compare } from "@foxglove/rostime";
-import { estimateObjectSize } from "@foxglove/studio-base/players/messageMemoryEstimation";
-import {
-  PlayerProblem,
-  MessageEvent,
-  Topic,
-  TopicStats,
-} from "@foxglove/studio-base/players/types";
+import { MessageEvent, PlayerProblem, TopicStats } from "@foxglove/studio-base/players/types";
 import { RosDatatypes } from "@foxglove/studio-base/types/RosDatatypes";
 import BrowserHttpReader from "@foxglove/studio-base/util/BrowserHttpReader";
 import CachedFilelike from "@foxglove/studio-base/util/CachedFilelike";
@@ -22,22 +15,24 @@ import Bzip2 from "@foxglove/wasm-bz2";
 import decompressLZ4 from "@foxglove/wasm-lz4";
 
 import {
-  IIterableSource,
-  IteratorResult,
-  Initalization,
-  MessageIteratorArgs,
   GetBackfillMessagesArgs,
+  ISerializedIterableSource,
+  Initalization,
+  IteratorResult,
+  MessageIteratorArgs,
+  TopicWithDecodingInfo,
 } from "./IIterableSource";
 
 type BagSource = { type: "file"; file: File } | { type: "remote"; url: string };
 
-export class BagIterableSource implements IIterableSource {
+export class BagIterableSource implements ISerializedIterableSource {
   readonly #source: BagSource;
 
   #bag: Bag | undefined;
-  #readersByConnectionId = new Map<number, MessageReader>();
   #datatypesByConnectionId = new Map<number, string>();
-  #messageSizeEstimateByTopic: Record<string, number> = {};
+  #textEncoder = new TextEncoder();
+
+  public readonly sourceType = "serialized";
 
   public constructor(source: BagSource) {
     this.#source = source;
@@ -106,7 +101,7 @@ export class BagIterableSource implements IIterableSource {
     });
 
     const datatypes: RosDatatypes = new Map();
-    const topics = new Map<string, Topic>();
+    const topics = new Map<string, TopicWithDecodingInfo>();
     const topicStats = new Map<string, TopicStats>();
     const publishersByTopic: Initalization["publishersByTopic"] = new Map();
     for (const [id, connection] of this.#bag.connections) {
@@ -132,7 +127,13 @@ export class BagIterableSource implements IIterableSource {
       }
 
       if (!existingTopic) {
-        topics.set(connection.topic, { name: connection.topic, schemaName });
+        topics.set(connection.topic, {
+          name: connection.topic,
+          schemaName,
+          messageEncoding: "ros1",
+          schemaData: this.#textEncoder.encode(connection.messageDefinition),
+          schemaEncoding: "ros1msg",
+        });
       }
 
       // Update the message count for this topic
@@ -142,8 +143,6 @@ export class BagIterableSource implements IIterableSource {
       topicStats.set(connection.topic, { numMessages });
 
       const parsedDefinition = parseMessageDefinition(connection.messageDefinition);
-      const reader = new MessageReader(parsedDefinition);
-      this.#readersByConnectionId.set(id, reader);
 
       for (const definition of parsedDefinition) {
         // In parsed definitions, the first definition (root) does not have a name as is meant to
@@ -172,13 +171,13 @@ export class BagIterableSource implements IIterableSource {
 
   public async *messageIterator(
     opt: MessageIteratorArgs,
-  ): AsyncIterableIterator<Readonly<IteratorResult>> {
+  ): AsyncIterableIterator<Readonly<IteratorResult<Uint8Array>>> {
     yield* this.#messageIterator({ ...opt, reverse: false });
   }
 
   async *#messageIterator(
     opt: MessageIteratorArgs & { reverse: boolean },
-  ): AsyncGenerator<Readonly<IteratorResult>> {
+  ): AsyncGenerator<Readonly<IteratorResult<Uint8Array>>> {
     if (!this.#bag) {
       throw new Error("Invariant: uninitialized");
     }
@@ -191,10 +190,8 @@ export class BagIterableSource implements IIterableSource {
       start: opt.start,
     });
 
-    const readersByConnectionId = this.#readersByConnectionId;
     for await (const bagMsgEvent of iterator) {
       const connectionId = bagMsgEvent.connectionId;
-      const reader = readersByConnectionId.get(connectionId);
 
       if (end && compare(bagMsgEvent.timestamp, end) > 0) {
         return;
@@ -214,49 +211,29 @@ export class BagIterableSource implements IIterableSource {
         return;
       }
 
-      if (reader) {
-        // bagMsgEvent.data is a view on top of the entire chunk. To avoid keeping references for
-        // chunks (which will fill up memory space when we cache messages) when make a copy of the
-        // data.
-        const dataCopy = bagMsgEvent.data.slice();
-        const parsedMessage = reader.readMessage(dataCopy);
+      // bagMsgEvent.data is a view on top of the entire chunk. To avoid keeping references for
+      // chunks (which will fill up memory space when we cache messages) when make a copy of the
+      // data.
+      const dataCopy = bagMsgEvent.data.slice();
 
-        // Lookup the size estimate for this topic or compute it if not found in the cache.
-        let msgSizeEstimate = this.#messageSizeEstimateByTopic[bagMsgEvent.topic];
-        if (msgSizeEstimate == undefined) {
-          msgSizeEstimate = estimateObjectSize(parsedMessage);
-          this.#messageSizeEstimateByTopic[bagMsgEvent.topic] = msgSizeEstimate;
-        }
-
-        yield {
-          type: "message-event",
-          msgEvent: {
-            topic: bagMsgEvent.topic,
-            receiveTime: bagMsgEvent.timestamp,
-            sizeInBytes: Math.max(bagMsgEvent.data.byteLength, msgSizeEstimate),
-            message: parsedMessage,
-            schemaName,
-          },
-        };
-      } else {
-        yield {
-          type: "problem",
-          connectionId,
-          problem: {
-            severity: "error",
-            message: `Cannot deserialize message for missing connection id ${connectionId}`,
-            tip: `Check that your bag file is well-formed. It should have a connection record for every connection id referenced from a message record.`,
-          },
-        };
-      }
+      yield {
+        type: "message-event",
+        msgEvent: {
+          topic: bagMsgEvent.topic,
+          receiveTime: bagMsgEvent.timestamp,
+          sizeInBytes: dataCopy.byteLength,
+          message: dataCopy,
+          schemaName,
+        },
+      };
     }
   }
 
   public async getBackfillMessages({
     topics,
     time,
-  }: GetBackfillMessagesArgs): Promise<MessageEvent[]> {
-    const messages: MessageEvent[] = [];
+  }: GetBackfillMessagesArgs): Promise<MessageEvent<Uint8Array>[]> {
+    const messages: MessageEvent<Uint8Array>[] = [];
     for (const entry of topics.entries()) {
       // NOTE: An iterator is made for each topic to get the latest message on that topic.
       // An single iterator for all the topics could result in iterating through many

packages/studio-base/src/players/IterablePlayer/BagIterableSourceWorker.worker.ts
@@ -5,18 +5,18 @@
 import * as Comlink from "comlink";
 
 import { IterableSourceInitializeArgs } from "@foxglove/studio-base/players/IterablePlayer/IIterableSource";
-import { WorkerIterableSourceWorker } from "@foxglove/studio-base/players/IterablePlayer/WorkerIterableSourceWorker";
+import { WorkerRawIterableSourceWorker } from "@foxglove/studio-base/players/IterablePlayer/WorkerRawIterableSourceWorker";
 
 import { BagIterableSource } from "./BagIterableSource";
 
-export function initialize(args: IterableSourceInitializeArgs): WorkerIterableSourceWorker {
+export function initialize(args: IterableSourceInitializeArgs): WorkerRawIterableSourceWorker {
   if (args.file) {
     const source = new BagIterableSource({ type: "file", file: args.file });
-    const wrapped = new WorkerIterableSourceWorker(source);
+    const wrapped = new WorkerRawIterableSourceWorker(source);
     return Comlink.proxy(wrapped);
   } else if (args.url) {
     const source = new BagIterableSource({ type: "remote", url: args.url });
-    const wrapped = new WorkerIterableSourceWorker(source);
+    const wrapped = new WorkerRawIterableSourceWorker(source);
     return Comlink.proxy(wrapped);
   }
 

packages/studio-base/src/players/IterablePlayer/IIterableSource.ts
@@ -218,7 +218,7 @@ export type IterableSourceInitializeArgs = {
  * Interface for a raw iterable source where messages are in their serialized byte form (Uint8Arrays).
  * A raw source is well suited for workers as array buffers can be efficientely transferred to the main thread.
  */
-export type IRawIterableSource = IIterableSource<Uint8Array> & { sourceType: "serialized" };
+export type ISerializedIterableSource = IIterableSource<Uint8Array> & { sourceType: "serialized" };
 
 /**
  * Interface for a deserialized iterable source where messages are in their deserialized form (unknown).

packages/studio-base/src/players/IterablePlayer/IterablePlayer.ts
@@ -44,7 +44,11 @@ import delay from "@foxglove/studio-base/util/delay";
 import { BlockLoader } from "./BlockLoader";
 import { BufferedIterableSource } from "./BufferedIterableSource";
 import { DeserializingIterableSource } from "./DeserializingIterableSource";
-import { IDeserializedIterableSource, IRawIterableSource, IteratorResult } from "./IIterableSource";
+import {
+  IDeserializedIterableSource,
+  ISerializedIterableSource,
+  IteratorResult,
+} from "./IIterableSource";
 
 const log = Log.getLogger(__filename);
 
@@ -77,7 +81,7 @@ const EMPTY_ARRAY = Object.freeze([]);
 type IterablePlayerOptions = {
   metricsCollector?: PlayerMetricsCollectorInterface;
 
-  source: IDeserializedIterableSource | IRawIterableSource;
+  source: IDeserializedIterableSource | ISerializedIterableSource;
 
   // Optional player name
   name?: string;
@@ -161,7 +165,7 @@ export class IterablePlayer implements Player {
   #problemManager = new PlayerProblemManager();
 
   // Unbuffered source, used as input source for buffered source and block loader.
-  #iterableSource: IDeserializedIterableSource | IRawIterableSource;
+  #iterableSource: IDeserializedIterableSource | ISerializedIterableSource;
 
   // Buffered source used for playback.
   #bufferedSource: IDeserializedIterableSource;

commit 8a2947b9c038f88873b2030bdb637d7a2f7bfd5e

Fix rosbag2 (db3) deserialization errors (#7455)
**User-Facing Changes**
None (regression hasn't been released yet)

**Description**
Fixes a regression introduced in #7420 which changed the way rosbag2
message are deserialized. The gendep-like message definition that we had
to generate for `parseChannel` contained ROS1 message definition which
can be quite different than their ROS2 counterparts (e.g.
std_msgs/Header contains an additional `seq uint32` field). This PR
makes sure that only ROS2 message definitions are part of the generated
message definition string.

packages/studio-base/src/players/IterablePlayer/rosdb3/RosDb3IterableSource.ts
@@ -2,6 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import { MessageDefinition } from "@foxglove/message-definition";
 import { ROS2_TO_DEFINITIONS, Rosbag2, SqliteSqljs } from "@foxglove/rosbag2-web";
 import { stringify } from "@foxglove/rosmsg";
 import { Time, add as addTime } from "@foxglove/rostime";
@@ -18,6 +19,14 @@ import {
   MessageIteratorArgs,
   TopicWithDecodingInfo,
 } from "../IIterableSource";
+
+function dataTypeToFullName(dataType: string): string {
+  const parts = dataType.split("/");
+  if (parts.length === 2) {
+    return `${parts[0]!}/msg/${parts[1]!}`;
+  }
+  return dataType;
+}
 
 export class RosDb3IterableSource implements ISerializedIterableSource {
   #files: File[];
@@ -78,7 +87,7 @@ export class RosDb3IterableSource implements ISerializedIterableSource {
         topicStats.set(topicDef.name, { numMessages });
       }
 
-      const parsedMsgdef = datatypes.get(topicDef.type);
+      const parsedMsgdef = ROS2_TO_DEFINITIONS.get(topicDef.type);
       if (parsedMsgdef == undefined) {
         problems.push({
           severity: "warn",
@@ -88,23 +97,28 @@ export class RosDb3IterableSource implements ISerializedIterableSource {
       } else {
         // Create the full gendeps-like message definition so that parseChannel() can parse it.
         const typesToProcess = [parsedMsgdef];
-        const typesForMessage: RosDatatypes = new Map();
+        const typesForMessage: MessageDefinition[] = [];
+        const seenTypes = new Set<string>();
         while (typesToProcess.length > 0) {
           const rosType = typesToProcess.shift()!;
+          typesForMessage.push(rosType);
           for (const { type, isComplex } of rosType.definitions) {
-            if (isComplex === true && !typesForMessage.has(type)) {
-              const newComplexType = datatypes.get(type);
+            const fullTypeName = dataTypeToFullName(type);
+            if (isComplex === true && !seenTypes.has(fullTypeName)) {
+              const newComplexType = ROS2_TO_DEFINITIONS.get(fullTypeName);
               if (!newComplexType) {
                 // Should in theory never happen as these are all well-known types
-                throw new Error(`invariant: Subtype ${type} of type ${rosType.name} not found.`);
+                throw new Error(
+                  `invariant: Subtype ${fullTypeName} of type ${rosType.name} not found.`,
+                );
               }
               typesToProcess.push(newComplexType);
-              typesForMessage.set(type, newComplexType);
+              seenTypes.add(fullTypeName);
             }
           }
         }
 
-        const messageDefinition = stringify(Array.from(typesForMessage.values()));
+        const messageDefinition = stringify(typesForMessage);
         topic.schemaData = this.#textEncoder.encode(messageDefinition);
         topic.schemaEncoding = "ros2msg";
         topics.push(topic);