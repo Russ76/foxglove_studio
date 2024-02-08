51875347418267475141bc5da4de837262f17751
Roman Shtylman <roman@foxglove.dev>
2024-02-07 22:01:33 -0800

    fix: foxglove websocket player incorrect startTime (#7449)
    This PR updates the client `time` event handler to re-set the
    `startTime` and `endTime` on the first setting of `clockTime`. Since
    `clockTime` will be used for subsequent calls to `getCurrentTime` this
    results in a `startTime` that is properly updated to a server specified
    clock time instead of `ZERO_TIME`.
    
    Fixes: #7406

--- packages/studio-base/src/players/FoxgloveWebSocketPlayer/index.ts
+++ packages/studio-base/src/players/FoxgloveWebSocketPlayer/index.ts
@@ -599,6 +599,13 @@ export default class FoxgloveWebSocketPlayer implements Player {
         this.#parsedMessagesBytes = 0;
       }
 
+      // Override any previous start/end time when we set a clockTime for the first time which means
+      // we've received the first "time" event and know the server controlled time.
+      if (!this.#clockTime) {
+        this.#startTime = time;
+        this.#endTime = time;
+      }
+
       this.#clockTime = time;
       this.#emitState();
     });
@@ -1285,6 +1292,7 @@ export default class FoxgloveWebSocketPlayer implements Player {
   }
 
   #resetSessionState(): void {
+    log.debug("Reset session state");
     this.#startTime = undefined;
     this.#endTime = undefined;
     this.#clockTime = undefined;
